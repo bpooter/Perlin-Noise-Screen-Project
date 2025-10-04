e987aeda2feebd5a370808e95cf59092c8330566
bpooter <brandonpotter9225@yahoo.com>
2025-10-04 17:12:12 -0400

    rendered a test rotating 3d cube

--- src/App.java
+++ src/App.java
@@ -1,7 +1,14 @@
+import javafx.animation.Animation;
 import javafx.animation.PauseTransition;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -14,9 +21,22 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
 
-        Scene scene = new Scene(root, 800,600);
+        Box cube = new Box(200,200,200);
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(Color.DEEPSKYBLUE);
+        material.setSpecularColor(Color.LIGHTBLUE);
+        cube.setMaterial(material);
+
+        Group root = new Group(cube);
+
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-600);
+        camera.setNearClip(0.1);
+        camera.setFarClip(2000.0);
+
+        Scene scene = new Scene(root, 800,600, true);
+        scene.setCamera(camera);
 
         PauseTransition delay = new PauseTransition(Duration.seconds(1));
         delay.setOnFinished(event -> readyToExit = true);
@@ -40,6 +60,13 @@ public class App extends Application {
         stage.setFullScreen(true);
 
         stage.show();
+
+        RotateTransition rotateX = new RotateTransition(Duration.seconds(10), cube);
+        rotateX.setAxis(Rotate.X_AXIS);
+        rotateX.setByAngle(360);
+        rotateX.setCycleCount(Animation.INDEFINITE);
+        rotateX.play();
+
     }
 
     public static void main(String[] args) {
