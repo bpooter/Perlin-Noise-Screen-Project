3811570ccde6f50bcadedc26c59251af203e439f
bpooter <brandonpotter9225@yahoo.com>
2025-10-04 18:04:25 -0400

    rendered a test rotating 3d cube

--- src/App.java
+++ src/App.java
@@ -2,12 +2,10 @@ import javafx.animation.Animation;
 import javafx.animation.PauseTransition;
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
+import javafx.scene.shape.*;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -22,13 +20,49 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Box cube = new Box(200,200,200);
+        Box cube = new Box(100,100,100);
         PhongMaterial material = new PhongMaterial();
-        material.setDiffuseColor(Color.DEEPSKYBLUE);
-        material.setSpecularColor(Color.LIGHTBLUE);
+        material.setDiffuseColor(Color.DARKGRAY);
+        material.setSpecularColor(Color.WHEAT);
         cube.setMaterial(material);
 
-        Group root = new Group(cube);
+        TriangleMesh triangleMesh = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
+        float[] points = {
+                0.0f,0.0f,0.0f,
+                100.0f,0.0f,0.0f,
+                0.0f,100.0f,0.0f
+        };
+
+        triangleMesh.getPoints().addAll(points);
+
+        float[] texCoords = { 0.0f,0.0f };
+
+        triangleMesh.getTexCoords().addAll(texCoords);
+
+        int[] faces = {
+                0,0,1,0,2,0
+        };
+
+        triangleMesh.getFaces().addAll(faces);
+
+        MeshView meshView = new MeshView(triangleMesh);
+        meshView.setMaterial(material);
+
+        meshView.setCullFace(CullFace.NONE);
+        meshView.setTranslateX(-50);  // Center it (optional)
+        meshView.setTranslateY(-50);
+        meshView.setTranslateZ(0);    // Keep it at origin, in front of camera
+
+        Group root = new Group(meshView);
+
+        // Add some lights!
+        AmbientLight ambient = new AmbientLight(Color.rgb(100, 100, 100));
+        PointLight light = new PointLight(Color.WHITE);
+        light.setTranslateX(150);
+        light.setTranslateY(-100);
+        light.setTranslateZ(-300);
+
+        root.getChildren().addAll(ambient, light);
 
         PerspectiveCamera camera = new PerspectiveCamera(true);
         camera.setTranslateZ(-600);
@@ -61,11 +95,12 @@ public class App extends Application {
 
         stage.show();
 
-        RotateTransition rotateX = new RotateTransition(Duration.seconds(10), cube);
-        rotateX.setAxis(Rotate.X_AXIS);
-        rotateX.setByAngle(360);
-        rotateX.setCycleCount(Animation.INDEFINITE);
-        rotateX.play();
+        //setting the rotation to rotate along the y axis.
+        RotateTransition rotateY = new RotateTransition(Duration.seconds(10), cube);
+        rotateY.setAxis(Rotate.Y_AXIS);
+        rotateY.setByAngle(360);
+        rotateY.setCycleCount(Animation.INDEFINITE);
+        rotateY.play();
 
     }
 
