8b99d5193ce03a1143c3835943f49cfc4c338367
bpooter <brandonpotter9225@yahoo.com>
2025-10-06 18:51:11 -0400

    working on terrain generator class

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.*;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,11 +23,12 @@ public class App extends Application {
 
         TriangleMesh triangleMesh = new TriangleMesh(VertexFormat.POINT_TEXCOORD);
 
+        /* todo make the noise more deliberate, fix the noise to generate mountains valleys and plains rather than uniform */
         FastNoiseLite noise = new FastNoiseLite();
-        noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
-        noise.SetFrequency(0.1f);
+        noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+        noise.SetFrequency(0.005f);
         noise.SetFractalType(FastNoiseLite.FractalType.FBm);
-        noise.SetFractalOctaves(4);
+        noise.SetFractalOctaves(5);
         noise.SetFractalLacunarity(2.0f);
         noise.SetFractalGain(0.5f);
 
@@ -36,14 +38,50 @@ public class App extends Application {
         float scrollSpeed = 0.025f;
         final float[] scrollOffset = {0f};
 
+        int width = 300;
+        int depth = 300;
+
         int roadWidth = 3;
         int roadStart = (SIZE / 2) - (roadWidth / 2);
         int roadEnd = roadStart + roadWidth;
 
+        TerrainGenerator terrain = new TerrainGenerator();
+        double[][] heights = new double[width][depth];
+        //terrain.updateMeshHeights(heights, width, depth);
+        float spacing = 100f;
+
+        // add vertices to triangle mesh
+        for (int z = 0; z < depth - 1; z++){
+            for (int x = 0; x < width - 1; x++){
+                float worldX = (x - width / 2f) * spacing;
+                float worldY = (float) heights[x][z]; // height from terrain generator
+                float worldZ = (z - depth / 2f) * spacing;
+                triangleMesh.getPoints().addAll(worldX, worldY, worldZ);
+            }
+        }
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                scrollOffset[0] += scrollSpeed;
+                terrain.updateScroll();
+                terrain.updateMeshHeights(heights, width, depth);
+
+                // add vertices to triangle mesh with updated values
+                for (int z = 0; z < depth - 1; z++){
+                    for (int x = 0; x < width - 1; x++){
+                        float worldX = (x - width / 2f) * spacing;
+                        float worldY = (float) heights[x][z]; // height from terrain generator
+                        float worldZ = (z - depth / 2f) * spacing;
+                        triangleMesh.getPoints().addAll(worldX, worldY, worldZ);
+                    }
+                }
+            }
+        };
+
+        /*AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                //scrollOffset[0] += scrollSpeed;
 
                 triangleMesh.getPoints().clear();
 
@@ -66,7 +104,7 @@ public class App extends Application {
                     }
                 }
             }
-        };
+        };*/
         timer.start();
 
 
@@ -87,10 +125,10 @@ public class App extends Application {
         PhongMaterial filledMaterial = new PhongMaterial();
         PhongMaterial lineMaterial = new PhongMaterial();
 
-        filledMaterial.setDiffuseColor(Color.CYAN);
+        filledMaterial.setDiffuseColor(Color.BLACK);
         filledMaterial.setSpecularColor(Color.TRANSPARENT);
 
-        lineMaterial.setDiffuseColor(Color.BLACK);
+        lineMaterial.setDiffuseColor(Color.WHITE);
 
         MeshView meshView = new MeshView(triangleMesh);
         MeshView filledMesh = new MeshView(triangleMesh);
@@ -115,6 +153,12 @@ public class App extends Application {
 
         root.getChildren().addAll(ambient, light);
 
+        Box originBox = new Box(100,100,100);
+        originBox.setMaterial(new PhongMaterial(Color.RED));
+        originBox.setTranslateZ(-400);
+        root.getChildren().add(originBox);
+
+
         Scene scene = new Scene(root, 800,600, true);
 
         PerspectiveCamera camera = new PerspectiveCamera(true);
@@ -127,8 +171,11 @@ public class App extends Application {
 
         // Set camera at a distance so it sees the whole mesh horizontally
         camera.setTranslateZ(offsetZ - requiredDistance);
-        camera.setTranslateY(-50);
-        camera.setTranslateX(0);
+        camera.setRotationAxis(Rotate.X_AXIS);
+        //camera.setRotate(-10);
+        camera.setRotationAxis(Rotate.Y_AXIS);
+        camera.setRotate(-30);
+        camera.setTranslateZ(-1000);
         camera.setNearClip(0.1);
         camera.setFarClip(2000.0);
         scene.setCamera(camera);
--- src/TerrainGenerator.java
+++ src/TerrainGenerator.java
@@ -1,2 +1,98 @@
+
 public class TerrainGenerator {
+
+    // noise objects
+    private final FastNoiseLite baseNoise;
+    private final FastNoiseLite regionNoise;
+    private final FastNoiseLite warpX;
+    private final FastNoiseLite warpZ;
+
+    //Terrain configuration
+    private double noiseScale = 0.005;
+    private double heightScale = 120.0;
+    private double warpAmount = 40.0;
+    private double scrollOffset = 0.0;
+    private double scrollSpeed = 0.5;
+
+    public TerrainGenerator(){
+
+        // base noise for height
+        baseNoise = new FastNoiseLite();
+        baseNoise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+        baseNoise.SetFractalType(FastNoiseLite.FractalType.FBm);
+        baseNoise.SetFractalGain(0.5f);
+        baseNoise.SetFractalLacunarity(2.0f);
+        baseNoise.SetFractalOctaves(5);
+
+        //region noise controls plains hills and mountains
+        regionNoise = new FastNoiseLite();
+        regionNoise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+        regionNoise.SetFrequency(0.0008f);
+
+        //warping noises
+        warpX = new FastNoiseLite();
+        warpX.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+        warpX.SetFrequency(0.002f);
+
+        warpZ = new FastNoiseLite();
+        warpZ.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+        warpZ.SetFrequency(0.002f);
+    }
+
+    public double getTerrainHeight(double x, double z){
+
+        //apply scrolling to simulate foward motion
+        double scrolledZ = z + scrollOffset;
+
+        //domain warping
+        float warpedX = (float)(x * noiseScale + warpX.GetNoise((float)x, (float) scrolledZ) * warpAmount);
+        float warpedZ = (float)(scrolledZ * noiseScale + warpZ.GetNoise((float)x,(float)scrolledZ) * warpAmount);
+
+        // region mask
+        double region = regionNoise.GetNoise((float)x * 0.001f, (float)scrolledZ * 0.001f);
+
+        double frequency;
+        double amplitude;
+        int octaves;
+
+        if (region < -0.3){
+            // plains
+            frequency = 0.002f;
+            amplitude = 0.3f;
+            octaves = 3;
+        } else if (region < 0.3) {
+            //hills
+            frequency = 0.004f;
+            amplitude = 0.6f;
+            octaves = 4;
+        } else {
+            // mountains
+            frequency = 0.007f;
+            amplitude = 1.0f;
+            octaves = 5;
+        }
+        baseNoise.SetFrequency((float) frequency);
+        baseNoise.SetFractalOctaves(octaves);
+
+
+        double height = baseNoise.GetNoise(warpedX, warpedZ);
+        return height * heightScale * amplitude;
+    }
+
+    public void updateScroll(){
+        scrollOffset += scrollSpeed;
+    }
+
+    // call update scroll before this in animation loop.
+    public void updateMeshHeights(double[][] heights, int width, int depth){
+        for (int x = 0; x < width; x++){
+            for (int z = 0; z < depth; z++){
+                heights[x][z] = getTerrainHeight(x, z);
+            }
+        }
+    }
+
+    public void setScrollSpeed(double speed){this.scrollSpeed = speed;}
+    public void setNoiseScale(double scale){this.noiseScale = scale;}
+    public void setHeightScale(double scale){this.heightScale = scale;}
 }
